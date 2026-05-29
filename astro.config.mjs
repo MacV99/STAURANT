46dad7e19a779d560149960857c0003d3d97262c
Miguel Cuellar <90780787+MacV99@users.noreply.github.com>
2026-05-28 21:27:51 -0500

    ajustes del PWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,15 +18,15 @@ export default defineConfig({
         orientation: 'portrait-primary',
         icons: [
           {
-            src: '/img/icon.avif',
+            src: '/img/icon-192.png',
             sizes: '192x192',
-            type: 'image/avif',
+            type: 'image/png',
             purpose: 'any',
           },
           {
-            src: '/img/icon.avif',
+            src: '/img/icon-512.png',
             sizes: '512x512',
-            type: 'image/avif',
+            type: 'image/png',
             purpose: 'maskable',
           },
         ],
